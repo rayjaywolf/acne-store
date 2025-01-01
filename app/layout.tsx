632cf58ba6d7a07eeb73cb923b3d9b095e4786b2
rayjay8 <jintarehaan@gmail.com>
2025-01-01 16:50:33 +0530

    remove toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import { CartProvider } from 'components/cart/cart-context';
 import { Navbar } from 'components/layout/navbar';
-import { WelcomeToast } from 'components/welcome-toast';
 import { GeistSans } from 'geist/font/sans';
 import { getCart } from 'lib/shopify';
 import { ensureStartsWith } from 'lib/utils';
@@ -49,7 +48,6 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
           <main>
             {children}
             <Toaster closeButton />
-            <WelcomeToast />
           </main>
         </CartProvider>
       </body>
