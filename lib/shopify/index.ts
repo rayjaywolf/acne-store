4fe3cdc9dbf0d9acd17a90a5d1b36f9794f97863
rayjay8 <jintarehaan@gmail.com>
2025-01-01 17:10:32 +0530

    hidden

--- lib/shopify/index.ts
+++ lib/shopify/index.ts
@@ -330,10 +330,9 @@ export async function getCollections(): Promise<Collection[]> {
       path: '/search/all',
       updatedAt: new Date().toISOString()
     },
-    // Filter out the `hidden` collections.
-    // Collections that start with `hidden-*` need to be hidden on the search page.
+    // Filter out the `hidden` collections and the "Hidden: All" collection.
     ...reshapeCollections(shopifyCollections).filter(
-      (collection) => !collection.handle.startsWith('hidden')
+      (collection) => !collection.handle.startsWith('hidden') && collection.title !== 'Hidden: All'
     )
   ];
 
